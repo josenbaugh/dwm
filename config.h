918102782cfec9982353e9f478776ba9d55acf10
Joshua Osenbaugh <jaosenbaugh@sharedsvcs.com>
2021-04-03 08:57:35 -0400

    inital commit

--- config.h
+++ config.h
@@ -1,16 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 6;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 1;		/* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+// static const char *fonts[]          = { "DejaVuSansMono Nerd Font:size=8" };
+static const char *fonts[]          = { 
+	"Hack Nerd Font:size=8.5",
+	"Noto Color Emoji:pixelsize=10:antialias=true:autohint=true"
+};
+static const char dmenufont[]          = "Hack Nerd Font:size=8.5";
+// static const char dmenufont[]       = "DejaVuSansMono Nerd Fon:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -30,10 +35,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title					tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,					0,            1,           -1 },
+	{ NULL,       NULL,       "StatusCalendar",		0,            1,           -1 },
+    { "Firefox",  NULL,       NULL,					1 << 8,       0,           -1 },
+    { "Microsoft Teams - Preview",  "microsoft teams - preview",       "Microsoft Teams Notification",       1 << 8,       1,           -1 },
 };
+
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
@@ -60,41 +68,78 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = { "dmenu_run -p 'Run: '" };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+
+static const char *dwmblocksconfigcmd[] = { "st", "nvim", "code/dwmblocks/config.h", NULL };
+
 
 #include "selfrestart.c"
+#include <X11/XF86keysym.h>
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ Mod1Mask,                     XK_Return, zoom,           {0} },
+    { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } }, //open scratchpad
+	{ MODKEY|ShiftMask,             XK_t,      spawn,          SHCMD("todo") },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("scrotclip") },
+	{ MODKEY|ShiftMask,				XK_m,      spawn,          SHCMD("manmenu") },
+	{ MODKEY|ShiftMask,				XK_e,      spawn,          SHCMD("dmenuunicode") },
+	{ MODKEY,						XK_u,      spawn,          SHCMD("dmmount") },
+	{ MODKEY|ShiftMask,				XK_u,      spawn,          SHCMD("dmumount") },
+	{ MODKEY|ControlMask,			XK_u,      spawn,          SHCMD("dmumount -l") },
+	{ MODKEY|ShiftMask,				XK_c,      spawn,          SHCMD("calendar") },
+
+    /* Multimedia */
+	{ 0,                            XF86XK_AudioLowerVolume , spawn, SHCMD("amixer -q set Master 2%- unmute; kill -44 $(pidof dwmblocks)") },
+	{ 0,                            XF86XK_AudioRaiseVolume , spawn, SHCMD("amixer -q set Master 2%+ unmute; kill -44 $(pidof dwmblocks)") },
+	{ 0,                            XF86XK_AudioMute        , spawn, SHCMD("amixer -q set Master toggle; kill -44 $(pidof dwmblocks)") },
+	{ 0,                            XF86XK_MonBrightnessUp  , spawn, SHCMD("xbacklight -inc 2.5; kill -45 $(pidof dwmblocks)") },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn, SHCMD("xbacklight -dec 2.5; kill -45 $(pidof dwmblocks)") },
+	{ 0,                            XF86XK_Mail             , spawn, SHCMD("xbacklight -inc 2.5; kill -45 $(pidof dwmblocks)") }, //brightness up for velocifire
+	{ 0,                            XF86XK_Tools            , spawn, SHCMD("xbacklight -dec 2.5; kill -45 $(pidof dwmblocks)") }, //brightness up for velocifire
+
+    /* Window manipulation */
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, //send window to tag
+	{ MODKEY,                       XK_c,      killclient,     {0} }, //kill window
+
+    /* Stack manipulation */
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } }, //move up through window stack
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } }, //move down through window stack
+	{ MODKEY,                       XK_space,  focusstack,     {.i = -1 } }, //move down through window stack
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} }, //resize horizontally
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} }, //resize horizontally
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,						XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+
+    /* Layout manipulation */
+	{ MODKEY,                       XK_Tab,    view,           {0} }, //back and forth between views
+	{ MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
+	{ MODKEY|ControlMask,           XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+
+    /* Monitor manipulation */
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-    { MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+    /* dwm controls */
+	{ MODKEY,                       XK_q,      spawn,          SHCMD("light-locker-command -l") }, //lock screen
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} }, //quit out of dwm
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} }, //restart dwm
+
+    /* Tag Keys */
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } }, //view all tags
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -116,6 +161,10 @@ static Button buttons[] = {
 	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
 	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
 	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
+	{ ClkStatusText,        0,              Button4,        sigdwmblocks,   {.i = 4} },
+	{ ClkStatusText,        0,              Button5,        sigdwmblocks,   {.i = 5} },
+	{ ClkStatusText,        ShiftMask,      Button1,        sigdwmblocks,   {.i = 6} },
+	{ ClkStatusText,        ShiftMask,      Button3,        spawn,          SHCMD("st -d ~/code/dwmblocks nvim ~/code/dwmblocks/config.h") },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
@@ -124,3 +173,4 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
